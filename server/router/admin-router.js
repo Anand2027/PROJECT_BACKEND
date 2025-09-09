06bd822dce79ca31337317b167d1fbc1638ae96f
Anand2027 <anandgupta020204@gmail.com>
2025-09-09 14:01:27 +0530

    secure admin by jwt verification - 41

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const adminController = require("../controllers/admin-controller")             //  40.0
 const router = express.Router();
+const authMiddleware = require("../middlewares/auth-middleware")               //  41.0
 
 
-router.route("/users").get(adminController.getAllUsers)                     //  40.0
-router.route("/contacts").get(adminController.getAllContacts)            //  40.0 
+router.route("/users").get(authMiddleware, adminController.getAllUsers)                     //  41.0
+router.route("/contacts").get(authMiddleware, adminController.getAllContacts)            //  41.0
 
 module.exports = router;
